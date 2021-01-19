2094e3afe96244abc630db74483e1939f583e83c
Aaron Meads <aaron.meads@gmail.com>
2021-01-19 16:56:17 -0700

    Adding More Tests

---
+++ src/Todo/TodoList.test.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { render, fireEvent } from "@testing-library/react";
+import TodoList from "./TodoList";
+
+it("renders without crashing", function () {
+  render(<TodoList />);
+});
+
+it("matches snapshot", function () {
+  const { asFragment } = render(<TodoList />);
+  expect(asFragment()).toMatchSnapshot();
+});
+
+it("should add new box", function () {
+  const { queryByText, getByLabelText } = render(<TodoList />);
+  const input = getByLabelText("Todo");
+  const btn = queryByText("Create Todo");
+  expect(queryByText("Feed Chickens")).not.toBeInTheDocument();
+  fireEvent.change(input, { target: { value: "Feed Chickens" } });
+  fireEvent.click(btn);
+  expect(queryByText("Feed Chickens")).toBeInTheDocument();
+});
